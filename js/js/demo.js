b01064c31b514a69317746aff2faa3f1276e87ec
Dhaval Chaudhary <mr.spark2591@gmail.com>
2016-05-31 17:27:15 +0530

    added demo for bar width

--- js/js/demo.js
+++ js/js/demo.js
@@ -2,11 +2,13 @@ var quickDemoFlag = false;
 var quickDemo = function(){
 	if(!quickDemoFlag){
 		quickDemoFlag = true;
+		$('.pattern').attr("disabled", "disable");
 		progressBar.progressBarStart();
 		setTimeout(function(){
     		if(progressBar.inprogressFlag){
     			progressBar.progressBarStop();
     			quickDemoFlag = false;
+    			$('.pattern').removeAttr("disabled");
     		}
 		},5000);
 	}	
@@ -18,10 +20,12 @@ $('.quick-demo-button').click(function(){
 });
 $('.start-progress-demo-button').click(function(){
 	progressBar.progressBarStart();
+	$('.pattern').attr("disabled", "disable");
 });
 $('.stop-progress-demo-button').click(function(){
 	progressBar.progressBarStop();
 	quickDemoFlag = false;
+	$('.pattern').removeAttr("disabled");
 });
 $('.progressbar-color').change(function(){
 	configObject.color = $('.progressbar-color').val();
@@ -34,7 +38,14 @@ $('.progressbar-height').change(function(){
 $('.progressbar-interval').change(function(){
 	configObject.intervalAnmation = $('.progressbar-interval').val();
 	progressBar.progressBarStop();
+	$('.pattern').removeAttr("disabled");
 	quickDemoFlag = false;
+	progressBar.config(configObject);
+});
+$('.progressbar-barwidth').change(function(){
+	configObject.barWidth = $('.progressbar-barwidth').val();
+	progressBar.progressBarStop();
+	$('.pattern').removeAttr("disabled");
 	progressBar.config(configObject);
 });
 $('.pattern').change(function(){
@@ -43,9 +54,15 @@ $('.pattern').change(function(){
 		$('.pattern2').addClass('hide');
 		configObject.pattern = parseInt($('.pattern').val());
 		progressBar.config(configObject);
+		if($('.pattern').val() == 3){
+			$('.pattern3').removeClass('hide');
+		}else{
+			$('.pattern3').addClass('hide');
+		}
 	}
 	if($('.pattern').val() == 2){
 		$('#pattern1').slideUp();
+		$('.pattern3').addClass('hide');
 		$('.pattern2').removeClass('hide');
 		configObject.pattern = 1;
 		progressBar.config(configObject);
